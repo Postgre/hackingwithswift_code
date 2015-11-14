f0292fcabf3c6999e4cb613e6f7c44b247e624d0
lovelock <frostwong@gmail.com>
2015-11-14 15:53:25 +0800

    add project4

---
+++ Project3/Project1/DetailViewController.swift
@@ -0,0 +1,67 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by frost on 11/13/15.
+//  Copyright © 2015 Unixera. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class DetailViewController: UIViewController {
+
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "shareTapped")
+
+    }
+    
+    func shareTapped() {
+//        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        let vc = SLComposeViewController(forServiceType: SLServiceTypeSinaWeibo)
+        vc.setInitialText("Look at this great picture!")
+        vc.addImage(detailImageView.image!)
+        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+        presentViewController(vc, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by frost on 11/14/15.
+//  Copyright © 2015 Unixera. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+//        let url = NSURL(string: "https://www.apple.com")!
+//        webView.loadRequest(NSURLRequest(URL: url))
+//        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction! = nil) {
+        let url = NSURL(string: "https://" + action.title!)
+        webView.loadRequest(NSURLRequest(URL: url!))
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
