6db5a7757776aa845d555b013e77d8a03f95a903
lovelock <frostwong@gmail.com>
2015-11-14 16:41:08 +0800

    update

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -28,6 +29,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = NSURL(string: "https://" + websites[0])
+        webView.loadRequest(NSURLRequest(URL: url!))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
         
         progressView = UIProgressView(progressViewStyle: .Default)
@@ -42,20 +45,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
     }
     
-    func refreshTapped() {
-        NSLog("gggg")
-        webView.reload()
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.URL
+        
+        if let host = url!.host {
+            for website in websites {
+                if host.rangeOfString(website) != nil {
+                    decisionHandler(.Allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.Cancel)
     }
+    
     func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
-        ac.addAction(UIAlertAction(title: "www.baidu.com", style: .Default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift", style: .Default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .Default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         presentViewController(ac, animated: true, completion: nil)
     }
     
-    func openPage(action: UIAlertAction! = nil) {
+    func openPage(action: UIAlertAction!) {
         let url = NSURL(string: "https://" + action.title!)
         webView.loadRequest(NSURLRequest(URL: url!))
     }
